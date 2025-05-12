142d0a2130995dce844c126a9090f90170a232f9
mahyar naeimi <mn.mahyar.naeimi@gmail.com>
2025-05-12 17:26:45 -0400

    import 2

--- squares.py
+++ squares.py
@@ -1,4 +1,8 @@
-from functions import square
+#from functions import square
 
-for i in range(10):
-    print(f"The square of {i} is {square(i)}")
+#for i in range(10):
+ #   print(f"The square of {i} is {square(i)}")
+
+import functions
+for i in range(90):
+    print(f"The square of {i} is {functions.square(i)}")
