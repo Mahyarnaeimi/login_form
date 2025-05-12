fbc6561dd05cbbe7d1e5a612b929bcae462db587
mahyar naeimi <mn.mahyar.naeimi@gmail.com>
2025-05-12 12:40:12 -0400

    sets.py

--- kick off.py
+++ kick off.py
@@ -1,1 +1,10 @@
-print("hello world")
+n = float(input("Type a number:"))
+
+if n > 0:
+    print("n is positive.")
+elif n < 0:
+    print("n is negative.")
+else:
+    print("n is zero.")
+
+input("press Enter to exit...")
